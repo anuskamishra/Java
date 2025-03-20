69e392c7d4502ba25896c994c8ce89d447d83b62
anuskamishra <mishranuska@gmail.com>
2025-03-20 11:15:20 +0530

    java code

---
+++ src/array2D/Array2D.java
@@ -0,0 +1,32 @@
+package array2D;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Array2D {
+	public static void main(String[] args) {
+		int[][] arr = new int[3][3];
+		Scanner scan = new Scanner(System.in);
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = scan.nextInt();
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+
+			System.out.println();
+		}
+
+		System.out.println(Arrays.deepToString(arr));
+		for (int[] row : arr) {   
+            for (int value : row) { 
+                System.out.print(value + " ");
+            }
+            System.out.println(); 
+        }
+	}
+}
