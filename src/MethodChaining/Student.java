8b49be70f21b7be06e17a5a8e07f32742158c86f
anuskamishra <mishranuska@gmail.com>
2025-03-25 12:24:08 +0530

    inheritance

--- src/MethodChaining/Student.java
+++ src/MethodChaining/Student.java
@@ -1,5 +1,15 @@
 package MethodChaining;
 
 public class Student {
-
+String name;
+int id;
+public Student setName(String name) {
+	this.name = name;
+	return this;//address of currently addressing object
+	
+}
+public void display() {
+	System.out.println(this.name+"  "+this.id);
+	
+}
 }
--- src/MethodChaining/StudentDriver.java
+++ src/MethodChaining/StudentDriver.java
@@ -1,5 +1,8 @@
 package MethodChaining;
 
 public class StudentDriver {
-
+public static void main(String[] args) {
+	Student s = new Student();
+	s.setName("Rohit");
+}
 }
